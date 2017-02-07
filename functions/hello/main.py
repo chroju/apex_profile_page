bafca2e150dbd6d8b3b64974348a1d023d5d8fbb
chroju <chor.chroju@gmail.com>
2017-02-07 15:49:36 +0900

    :hatching_chick: Initial commit
    Changes to be committed:
    new file:   .gitignore
    new file:   functions/hello/main.py
    new file:   functions/hello/requirements.txt
    new file:   functions/hello/templates/index.html.j2
    new file:   project.json

---
+++ functions/hello/main.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+from os import path
+from logging import getLogger
+from jinja2 import Environment, FileSystemLoader
+import feedparser
+import boto3
+
+COUNTER_HASH_SIZE = 10
+
+TECH_BLOG_URL = 'https://chroju.github.io/atom.xml'
+HOBBY_BLOG_URL = 'http://chroju.hatenablog.jp/feed'
+
+BUCKET = 'chroju-profile'
+s3 = boto3.client('s3')
+
+logger = getLogger()
+env = Environment(loader=FileSystemLoader(path.join(path.dirname(__file__), 'templates'), encoding='utf8'))
+
+def upload_s3(r):
+    key = 'index.html'
+    template = env.get_template('index.html.j2')
+    body = template.render(techblog=techblog, hobbyblog=hobbyblog)
+    s3.put_object(Bucket=BUCKET, Key=key, Body=body, ContentType='text/html')
+
+def pick_up_entries(url):
+    response = feedparser.parse(url)
+    result = []
+    for i in range(3):
+        result.append(
+            { "url" : response.entries[i].link,
+              "title" : response.entries[i].title})
+    return result
+
+techblog = pick_up_entries(TECH_BLOG_URL)
+hobbyblog = pick_up_entries(HOBBY_BLOG_URL)
+
+def handle(event, context):
+    logger.info(event)
+    upload_s3(event)
+
