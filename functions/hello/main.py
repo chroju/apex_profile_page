5b77b4e48c7e5d7b39b7da33aeea8bde8229e937
chroju <chor.chroju@gmail.com>
2017-09-04 22:37:48 +0900

    いつぞやのcommit漏れ

--- functions/hello/main.py
+++ functions/hello/main.py
@@ -4,29 +4,32 @@ from logging import getLogger
 from jinja2 import Environment, FileSystemLoader
 import feedparser
 import boto3
+import logging
 
 COUNTER_HASH_SIZE = 10
 
 TECH_BLOG_URL = 'https://chroju.github.io/atom.xml'
 HOBBY_BLOG_URL = 'http://chroju.hatenablog.jp/feed'
-QIITA_URL = 'http://qiita.com/chroju'
+QIITA_URL = 'http://qiita.com/chroju/feed'
 
 BUCKET = 'chroju-profile'
 s3 = boto3.client('s3')
 
-logger = getLogger()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 env = Environment(loader=FileSystemLoader(path.join(path.dirname(__file__), 'templates'), encoding='utf8'))
 
 def upload_s3(r):
     key = 'index.html'
     template = env.get_template('index.html.j2')
-    body = template.render(techblog=techblog, hobbyblog=hobbyblog)
+    body = template.render(techblog=techblog, hobbyblog=hobbyblog, qiita=qiita)
     s3.put_object(Bucket=BUCKET, Key=key, Body=body, ContentType='text/html')
 
 def pick_up_entries(url):
     response = feedparser.parse(url)
     result = []
     for i in range(3):
+        logger.info(response.entries[i].title)
         result.append(
             { "url" : response.entries[i].link,
               "title" : response.entries[i].title})
